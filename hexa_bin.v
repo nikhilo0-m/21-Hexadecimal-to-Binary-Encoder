1a99474a1fad41be8ebb8e46e86f5d3a029d4d31
Nikhil Kumar Maharana <nikhilmaharana53@gmail.com>
2026-06-10 14:53:45 +0530

    Add files via upload

---
+++ hexa_bin.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+module hexa_bin(
+    input [15:0] in,
+    output [3:0] out
+    );
+    
+    assign out[3]= in[8] | in[9] | in[10] | in[11] | in[12] | in[13] | in[14] |in[15];
+    assign out[2]= in[4] | in[5] | in[6] | in[7] | in[12] | in[13] | in[14] | in[15];
+    assign out[1]= in[2] | in[3] | in[6] | in[7] | in[10] | in[11] | in[14] | in[15];
+    assign out[0]= in[1] | in[3] | in[5] | in[7] | in[9] | in[11] | in[13] |in[15];
+    
+endmodule
